49ea424b2f25f149d88f85b8b3f45a60bd712dd2
wsf <way1397@163.com>
2018-07-09 18:51:20 +0800

    1.练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
@@ -21,6 +21,7 @@ import static com.hencoder.hencoderpracticedraw6.Utils.dpToPixel;
 public class Practice01Translation extends RelativeLayout {
     Button animateBt;
     ImageView imageView;
+    int State = 0;
 
     public Practice01Translation(Context context) {
         super(context);
@@ -49,6 +50,30 @@ public class Practice01Translation extends RelativeLayout {
             @Override
             public void onClick(final View v) {
                 // TODO 在这里处理点击事件，通过 View.animate().translationX/Y/Z() 来让 View 平移
+                switch (State) {
+                    case 0:
+                        imageView.animate().translationX(500);
+                        break;
+                    case 1:
+                        imageView.animate().translationX(0);
+                        break;
+                    case 2:
+                        imageView.animate().translationY(300);
+                        break;
+                    case 3:
+                        imageView.animate().translationY(0);
+                        break;
+                    case 4:
+                        imageView.animate().translationZ(100);
+                        break;
+                    case 5:
+                        imageView.animate().translationZ(0);
+                        break;
+                }
+                State++;
+                if (State == 6) {
+                    State = 0;
+                }
             }
         });
     }
